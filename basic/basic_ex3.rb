03c9fb0e9390404f6097fb5aeb399fcedb30fc62
ellis <byskyline2158@gmail.com>
2015-12-07 18:24:09 +0800

    basic_ex3

---
+++ basic/basic_ex3.rb
@@ -0,0 +1,11 @@
+movies = { apple: 1975,
+           bird: 2004,
+           cat: 2013,
+           dog: 2001,
+           evil: 1981 }
+
+puts movies[:apple]
+puts movies[:bird]
+puts movies[:cat]
+puts movies[:evil]
+puts movies[:dog]
