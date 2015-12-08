b4e2b4d678f3950d1420614f5629d249616efd73
ellis <byskyline2158@gmail.com>
2015-12-09 07:01:36 +0800

    classes_and_objects_part2.ex1

---
+++ classes_and_objects_part2/classes_and_objects_part2.ex1.rb
@@ -0,0 +1,10 @@
+class MyCar
+
+  # code omitted for brevity...
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+MyCar.gas_mileage(13, 351)  # => "27 miles per gallon of gas"
