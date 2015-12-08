a1b76ab38c4ee942731d77c1455e5f2128fe7154
ellis <byskyline2158@gmail.com>
2015-12-09 07:12:28 +0800

    inheritance_ex3

---
+++ inheritance/inheritance_ex3.rb
@@ -0,0 +1,32 @@
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+end
+
+class Vehicle
+  @@number_of_vehicles = 0
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles"
+  end
+
+  def initialize
+    @@number_of_vehicles += 1
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+  #code omitted for brevity...
+end
+
+class MyTruck < Vehicle
+  include Towable
+
+  NUMBER_OF_DOORS = 2
+end
